e942fb55374fd131806076a78e0ba547253277bd
Evan Travers <evantravers@gmail.com>
2013-10-02 12:45:26 -0500

    create the search model
    going to try and store the resulting json as itself, parse on render.
    Might be too expensive.

--- db/migrate/20131002174037_create_searches.rb
+++ db/migrate/20131002174037_create_searches.rb
@@ -2,7 +2,7 @@ class CreateSearches < ActiveRecord::Migration
   def change
     create_table :searches do |t|
       t.string :vin
-      t.hash :attributes
+      t.text :attributes
 
       t.timestamps
     end
