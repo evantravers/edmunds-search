807e150f191117620590dbfc107b3edfbe7c5f71
Evan Travers <evantravers@gmail.com>
2013-10-03 10:24:26 -0500

    add a few more descriptive methods to the model

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,11 +2,19 @@ class Search < ActiveRecord::Base
   attr_reader :vin
   attr_accessor :vehicle_attributes, :vin
 
+  def vehicle_year
+    @vehicle_attributes['years'].first['year']
+  end
+
+  def vehicle_description
+    @vehicle_attributes['years'].first['styles'].first['name']
+  end
+
   # make magic methods to grab vehicle information
   def method_missing(method_name, *arguments, &block)
-    if method_name.to_s =~ /vehicle_(.*)/
+    if method_name.to_s =~ /\Avehicle_(.*)/
       begin
-        self.vehicle_attributes.fetch($1).fetch('name')
+        @vehicle_attributes.fetch($1).fetch('name')
       rescue KeyError
         super
       end
