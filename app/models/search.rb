ca4de748eeed2db78d1d978cc2e6bb153f9fc0a3
Evan Travers <evantravers@gmail.com>
2013-10-03 10:09:06 -0500

    make magic methods for vehicle information

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,17 @@
 class Search < ActiveRecord::Base
   attr_reader :vin
   attr_accessor :vehicle_attributes, :vin
+
+  # make magic methods to grab vehicle information
+  def method_missing(method_name, *arguments, &block)
+    if method_name.to_s =~ /vehicle_(.*)/
+      self.vehicle_attributes[$1]['name']
+    else
+      super
+    end
+  end
+
+  def respond_to_missing?(method_name, include_private = false)
+    method_name.to_s.start_with?('vehicle_') || super
+  end
 end
